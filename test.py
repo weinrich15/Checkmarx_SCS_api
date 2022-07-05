e11b6c7de707c5b1cf84c04c5bb3ad1dea410d39
Sean Casey <sean.casey@checkmarx.com>
2022-07-05 15:07:19 -0400

    Adding Dustico API Testing

--- test.py
+++ test.py
@@ -7,4 +7,20 @@ token = os.environ.get('DUSTICO_API_TOKEN')
 
 test = os.environ.get('TEST_SECRET')
 
+url = "https://api.dusti.co/v1/packages"
+
+headers = {
+    "Authorization": f"token {token}"
+}
+data = [
+    {
+        "name": "node-ipc",
+        "type": "npm",
+        "version": "9.2.2",
+    }
+]
+r = requests.post(url, json=data, headers=headers)
+r.raise_for_status()
+print(json.dumps(r.json(), indent=2))
+
 print(test)
